cf32a04bb1a4b035420a6d412e5d92ea402b30aa
Arthur De Araujo <wafflez180@yahoo.com>
2017-11-04 15:23:33 -0400

    Created CocoaPods

---
+++ LectureBuddy/Pods/Target Support Files/Pods-LectureBuddy/Pods-LectureBuddy-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LectureBuddy : NSObject
+@end
+@implementation PodsDummy_Pods_LectureBuddy
+@end
---
+++ LectureBuddy/Pods/Target Support Files/Pods-LectureBuddyUITests/Pods-LectureBuddyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LectureBuddyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_LectureBuddyUITests
+@end
